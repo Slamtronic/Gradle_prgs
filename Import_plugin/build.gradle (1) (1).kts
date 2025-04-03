e1a3bfe2462eaaf369cc1dec221b38e51eb1fce3
Slamtronic <139569102+Slamtronic@users.noreply.github.com>
2025-04-03 19:11:48 +0100

    Add files via upload

---
+++ Import_plugin/build.gradle (1) (1).kts
@@ -0,0 +1,13 @@
+val gloublBuild="my global build group"
+val ciBuild="my Ci build group"
+tasks.named<TaskReportTask>("tasks"){
+  displayGroup=gloublBuild
+}
+tasks.register("qualityCheckAll"){
+      group=gloublBuild
+      dependsOn(subprojects.map { ":${ it.name} : checkQuality"}  )       
+}
+tasks.register("CheckAll"){
+     group=ciBuild
+     dependsOn(subprojects.map { ":${ it.name} : check"} )       
+}
